b8294bdd654e70dbd6d641e152ab51dc2c50fa6d
denzalvin <ddennisalvin@gmail.com>
2022-06-27 13:24:28 -0700

    wrote generators for prime numbers and unique letters from a string

--- generator_excercises.py
+++ generator_excercises.py
@@ -2,11 +2,19 @@
 
 #Problem 1
 #Create a generator, primes_gen that generates prime numbers starting from 2.
+def primes_gen():
+    yield 2
+    yield from _primes_gen(3)
+
 
 gen = primes_gen()
 for _ in range(10):
-    print(next(gen), end=' ')
+    if gen % 2 == 0:
+        print(next(gen), end=' ')
+    
 # Expected output
+
+
 # 2 3 5 7 11 13 17 19 23 29
 #----------------------------------------------------------------------------
 
@@ -14,6 +22,10 @@ for _ in range(10):
 #Create a generator, unique_letters that generates unique letters from
 #the input string. It should generate the letters in the same order as
 #from the input string.
+def unique_letters(string):
+    for letter in string:
+        if letter not in string[string.index(letter)+1:]:
+            yield letter
 
 for letter in unique_letters('hello'):
     print(letter, end=' ')
